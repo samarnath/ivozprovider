093090e760b993b2d950d1901b6b09efd0db092f
Mikel Madariaga <mikel@irontec.com>
2017-07-19 15:40:59 +0200

    ApplicationServer logic migration

--- symfony/src/Ivoz/Domain/Model/ApplicationServer/PostPersistApplicationServer.php
+++ symfony/src/Ivoz/Domain/Model/ApplicationServer/PostPersistApplicationServer.php
@@ -1,32 +1,73 @@
 <?php
-namespace Ivoz\Application\ApplicationServer;
+namespace Ivoz\Domain\Model\ApplicationServer;
 
-use Kam\Domain\Model\Dispatcher as KamDispatcher;
+use Kam\Domain\Model\Dispatcher\Dispatcher as KamDispatcher;
 use Kam\Domain\Model\Dispatcher\DispatcherRepository as KamDispatcherRepository;
 use IvozProvider\Gearmand\Jobs\Xmlrpc;
-use Doctrine\Common\Persistence\ObjectManager;
-use Ivoz\Domain\Model\ApplicationServer\ApplicationServerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Core\Application\Command\CreateEntityFromDTO;
+use Core\Application\Command\UpdateEntityFromDTO;
 
 class PostPersistApplicationServer
 {
+    /**
+     * @var KamDispatcherRepository
+     */
     protected $dispatcherRepository;
+
+    /**
+     * @var Xmlrpc
+     */
     protected $xmlrpc;
+
+    /**
+     * @var EntityManagerInterface
+     */
     protected $em;
 
     public function __construct(
         KamDispatcherRepository $dispatcherRepository,
         Xmlrpc $xmlrpc,
-        ObjectManager $em
+        EntityManagerInterface $em,
+        CreateEntityFromDTO $createEntityFromDTO,
+        UpdateEntityFromDTO $entityUpdater
     ) {
         $this->dispatcherRepository = $dispatcherRepository;
         $this->xmlrpc = $xmlrpc;
         $this->em = $em;
+        $this->createEntityFromDTO = $createEntityFromDTO;
+        $this->entityUpdater = $entityUpdater;
     }
 
-
-    public function execute(ApplicationServerInterface)
+    public function execute(ApplicationServerInterface $entity)
     {
+        /**
+         * @var KamDispatcher $kamDispatcher
+         */
+        // Replicate ApplicationServer IP into kam_dispatcher
+        $kamDispatcher = $this->dispatcherRepository->findOneBy([
+            'applicationServer' => $entity->getId()
+        ]);
 
-        
+        $isNew = is_null($kamDispatcher);
+
+        $kamDispatcherDto = $isNew
+            ? KamDispatcher::createDTO()
+            : $kamDispatcher->toDTO();
+
+        $kamDispatcherDto
+            ->setApplicationServerId($entity->getId())
+            ->setSetid(1)
+//            ->setDestination('sip:' . $model->getIp() . ":6060")
+            ->setDescription($entity->getName());
+
+        if ($isNew) {
+            $this->createEntityFromDTO->execute(KamDispatcher::class, $kamDispatcherDto);
+        } else {
+            $this->entityUpdater->execute($kamDispatcher, $kamDispatcherDto);
+        }
+
+        $this->em->persist($kamDispatcher);
+        $this->em->flush();
     }
 }
